23b6f9b91a3667fba97ab8a286e3359f88332e7b
Suzette <suzetteislam@gmail.com>
2019-11-10 12:42:09 -0500

    feed page created

---
+++ client/feed/feed.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+})
+
+//get all posts function for the feed once user page is loaded
+const getAllPosts = () => {
+console.log('page has loaded');
+//create axios call to the users images and load them on the page
+}
+
+//get single posts
+
+const getSinglePost = () =>{
+    event.preventDefault();
+}
+
+// delete single post
+const deleteSinglePost = () =>{
+    event.preventDefault();
+    
+}
