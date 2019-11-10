c4819a23415348aebf24aed074629d3566c18f13
gsanchez13 <gisellesanchez@pursuit.org>
2019-11-10 16:00:38 -0500

    renamed and changed login functionality to register

---
+++ client/register_page/register.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    console.log("DOM Content Loaded");
+    // let loginForm = document.querySelector("#login");
+    // loginForm.addEventListener("submit", getUser);
+    let registerForm = document.querySelector("#register");
+    registerForm.addEventListener("submit", registerUser);
+})
+const getUser = (event) => {
+    event.preventDefault();
+    //reroutes to /users/:id endpoint
+    console.log("testing getUser axios request");
+}
+const registerUser = (event) => {
+    event.preventDefault();
+    console.log("testing registerUser axios request")
+}
